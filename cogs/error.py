1792b3bd38017deb93895d0ec7e04ad0904c1197
Sylvia Mills <me@chatalot.xyz>
2021-03-13 01:10:17 +0000

    Add additional information to command error discord message

--- cogs/error.py
+++ cogs/error.py
@@ -1,6 +1,7 @@
 import logging
 from os import getenv
 from traceback import format_exception
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -24,12 +25,23 @@ class ErrorCog(commands.Cog, name="Error Handling"):
         if isinstance(exc, ERRORS_TO_IGNORE):
             return
         if isinstance(exc, commands.CommandInvokeError):  # Bug found in code
+            # Generate additional error text:
+            additional_error_text = (
+                f"Exception Raised!\n"
+                f"Command: {ctx.command.qualified_name}\n"
+                f"Exception Text: {exc.original}\n"
+                f"Raised by user: `{ctx.author}`    `{ctx.author.id}`\n"
+                f"Timestamp: {datetime.now().isoformat()}\n"
+                f""
+            )
+
             # Generate paginated exception with traceback
             paginator = commands.Paginator(prefix="```py")
             for line in format_exception(type(exc), exc, exc.__traceback__):
                 paginator.add_line(line[:-1])  # The newline at the end is removed
 
-            async def send_pages(destination: discord.abc.Messageable) -> None:
+            async def send_formatted_exc(destination: discord.abc.Messageable) -> None:
+                await destination.send(additional_error_text)
                 for page in paginator.pages:
                     await destination.send(page)
 
@@ -38,7 +50,7 @@ class ErrorCog(commands.Cog, name="Error Handling"):
                     "An error occurred. Since you're an owner, "
                     "I'm DMing you the traceback."
                 )
-                await send_pages(ctx.author)
+                await send_formatted_exc(ctx.author)
 
             channel = self.bot.get_channel(int(getenv("ERROR_CHANNEL_ID")))
             if channel is None:
@@ -52,7 +64,7 @@ class ErrorCog(commands.Cog, name="Error Handling"):
                 "An error appeared in the command, attempting to report it now, thanks!"
             )
             try:
-                await send_pages(channel)
+                await send_formatted_exc(channel)
             except discord.Forbidden:
                 logging.error(
                     "Failed to report following exception, "
